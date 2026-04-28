327998490e777ad6ce04c93d05980d2479c0847e
Bame <ab89774@eid.utexas.edu>
2026-04-28 12:17:04 -0500

    Fixed burnup error and added tracking of physical flux histories

--- project_reactor v2.py
+++ project_reactor v2.py
@@ -27,68 +27,7 @@ refl=data['reflector']
 shld=data['shield']
 geo=data['geometry_cm_half_core']
 
-fuelmix_fast_BOL= {
-    'sigTr': 0.278,
-    'sigA': 0.01,
-    'nusigF': 0.005,
-    'kapsigF': 6.4e-14,
-    'sigS12':0.015}
-
-fuelmix_th_BOL={
-    'sigTr':0.833,
-    'sigA': 0.1,
-    'nusigF': 0.15,
-    'kapsigF': 1.9e-12,
-    'sigS12': 0}
-
-# reflector_xs_BOL = {
-#     'sigTr': 0.222,
-#     'sigA': 0.002,
-#     'nusigF': 0.0,
-#     'kapsigF': 0.0}
-
-reflector_fast_BOL = {
-    'sigTr': 0.303,
-    'sigA': 0.001,
-    'nusigF': 0,
-    'kapsigF': 0,
-    'sigS12': 0.04}
-
-reflector_th_BOL = {
-    'sigTr': 1.667,
-    'sigA': 0.02,
-    'nusigF': 0,
-    'kapsigF': 0,
-    'sigS12': 0}
-
-# shield_xs_BOL = {
-#     'sigTr': 0.417,
-#     'sigA': 0.050,
-#     'nusigF': 0.0,
-#     'kapsigF': 0.0}
-
-shield_fast_BOL = {
-    'sigTr':0.37,
-    'sigA': 0.05,
-    'nusigF': 0,
-    'kapsigF': 0,
-    'sigS12': 0.005,}
-
-shield_th_BOL = {
-    'sigTr': 1.111,
-    'sigA':0.5,
-    'nusigF': 0,
-    'kapsigF': 0,
-    'sigS12': 0}
-
 # EOL
-# fuelmix_xc_EOL = {
-#     'sigTr':   0.309,
-#     'sigA':    0.0684,   
-#     'nusigF':  0.0770,   
-#     'kapsigF': 5.270e-13
-# }
-
 fuelmix_fast_EOL={
     'sigTr': 0.84581,
     'sigA':0.01249,
@@ -103,12 +42,6 @@ fuelmix_th_EOL = {
     'kapsigF': 11.28578,
     'sigS12_thermal_to_fast': 0}
 
-# reflector_xs_EOL = {
-#     'sigTr': 0.222,
-#     'sigA': 0.002,
-#     'nusigF': 0.0,
-#     'kapsigF': 0.0}
-
 reflector_fast_EOL = {
     'sigTr': 0.31870373749771286,
     'sigA': 0.00,
@@ -122,12 +55,6 @@ reflector_th_EOL = {
     'nusigF': 0,
     'kapsigF': 0,
     'sigS12_thermal_to_fast': 0}
-
-# shield_xs_EOL = {
-#     'sigTr': 0.417,
-#     'sigA': 0.050,
-#     'nusigF': 0.0,
-#     'kapsigF': 0.0}
 
 sheild_fast_EOL = {
     'sigTr':0.6410438594515198,
@@ -154,7 +81,7 @@ def burnup(B_int,prop,speed):
             Sig_B=fuel[prop][0]
             Sig_E=fuelmix_fast_EOL[prop]
     else:
-        Sig_B=fuel[prop][0]
+        Sig_B=fuel[prop][1]
         Sig_E=fuelmix_th_EOL[prop]
 
     Sig_int=Sig_B+(Sig_E-Sig_B)/(B_EOL-B_BOL)*(B_int-B_BOL)
@@ -188,8 +115,10 @@ class Reactor1D:
         self.Dt_th = np.zeros(self.Nx)
         self.phi_f = np.ones(self.Nx)
         self.phi_th = np.ones(self.Nx)
+        self.phi_phys_f=np.ones(self.Nx)
+        self.phi_phys_th=np.ones(self.Nx)
         self.k = 1.0
-        self.history={'k': [], 'phi_f': [], 'phi_th': [], 'Bt': []}
+        self.history={'k': [], 'phi_f': [], 'phi_th': [], 'Bt': [], 'phi_phys_f': [], 'phi_phys_th': []}
         
         # FAST cross section vectors
         self.sigA_f=np.zeros(self.Nx)
@@ -263,7 +192,7 @@ class Reactor1D:
     def step(self, H, dt_days,tol=1E-6):
         self.get_xs(H)
         self.sig_R_1=self.sigA_f+self.sigS12_f
-        self.k,self.phi_f,self.phi_th=power_iteration(self.H, self.dx, 
+        self.k,self.phi_f,self.phi_th,self.phi_phys_f,self.phi_phys_th=power_iteration(self.H, self.dx, 
                                                       self.Dt_f,self.Dt_th, 
                                                       self.sig_R_1,self.sigA_th,
                                                       self.nusigF_f,self.nusigF_th,
@@ -277,6 +206,8 @@ class Reactor1D:
         self.history['phi_f'].append(self.phi_f.copy())
         self.history['phi_th'].append(self.phi_th.copy())
         self.history['Bt'].append(self.Bt.copy())
+        self.history['phi_phys_f'].append(self.phi_phys_f.copy())
+        self.history['phi_phys_th'].append(self.phi_phys_th.copy())
         pass
     
     def full_core(self,var,time_index=-1):
@@ -287,12 +218,6 @@ class Reactor1D:
 
 def power_iteration(H,dx,D_f,D_th,sig_R_1,sig_a_th,nu_sig_f,nu_sig_th,
                     kap_sig_f,kap_sig_th,sig_S12,tol,P,boundary):
-    # if isinstance(H,(list,tuple,np.ndarray)):
-    #     H_fuel,H_ref=H[0], H[1]
-    #     H_tot=sum(H)
-    #     N=int(H_tot/dx+1)
-    #     x=np.linspace(0,H_tot,N)
-    #     ifuel=H_fuel/dx
 
     H_tot=H
     N=int(H/dx+1)
@@ -303,8 +228,7 @@ def power_iteration(H,dx,D_f,D_th,sig_R_1,sig_a_th,nu_sig_f,nu_sig_th,
     # Build A_f
     A_f=np.zeros((N,N))
     for i in range(1,N-1):
-        # Dleft=(D_f[i]+D_f[i-1])/2
-        # Dright=(D_f[i]+D_f[i+1])/2
+
         # Harmonic Mean
         Dleft=(2*D_f[i]*D_f[i-1])/(D_f[i]+D_f[i-1])
         Dright=(2*D_f[i]*D_f[i+1])/(D_f[i]+D_f[i+1])
@@ -333,8 +257,7 @@ def power_iteration(H,dx,D_f,D_th,sig_R_1,sig_a_th,nu_sig_f,nu_sig_th,
     # Build A_th
     A_th=np.zeros((N,N))
     for i in range(1,N-1):
-        # Dleft=(D_th[i]+D_th[i-1])/2
-        # Dright=(D_th[i]+D_th[i+1])/2
+
         Dleft=(2*D_th[i]*D_th[i-1])/(D_th[i]+D_th[i-1])
         Dright=(2*D_th[i]*D_th[i+1])/(D_th[i]+D_th[i+1])
             
@@ -363,7 +286,7 @@ def power_iteration(H,dx,D_f,D_th,sig_R_1,sig_a_th,nu_sig_f,nu_sig_th,
     phi_th=np.sin(np.pi*x/(2*H_tot)) if boundary[0]=='vacuum' else np.ones(N)
     
     # Build A_tot matrix, phi_tot vector, nusigF matrix
-    sig_s=np.eye(N)*(-sig_S12)
+    sig_s=np.diag(-sig_S12)
     sig_s[0,0]=0
     sig_s[N-1,N-1]=0
     Zero=np.zeros((N,N))
@@ -402,33 +325,32 @@ def power_iteration(H,dx,D_f,D_th,sig_R_1,sig_a_th,nu_sig_f,nu_sig_th,
 
     phi_f=phi_tot[:N]
     phi_th=phi_tot[N:]    
+    conv_kapsig=3.2e-11
     norm_power_f=np.sum(kap_sig_f*phi_f*dx)
     norm_power_th=np.sum(kap_sig_th*phi_th*dx)
-    norm_power=norm_power_f+norm_power_th
+    norm_power=(norm_power_f+norm_power_th)*conv_kapsig
     scale_factor=P/norm_power
     phi_tot=phi_tot*scale_factor
-    return k_old,phi_f,phi_th
+    phi_phys_f=phi_tot[:N]
+    phi_phys_th = phi_tot[N:]
+    return k_old,phi_f,phi_th,phi_phys_f,phi_phys_th
 
 # 1D slab geometry:
     # vacuum [shield | reflector | core | reflector | shield] vacuum
     #          40       30         100 
 
 # Sizes for half the reactor
-# shield=40
-# reflector=30
-# fuel=50
 L=240 # Length of the reactor (240 Max)
 W=240 # Width of the reactor (240 Max)
 P_tot=1e6
 P_1D=P_tot/(L*W)
-# Size=([fuel,reflector,shield])
 Size=[int(geo['fuel']),int(geo['reflector']),int(geo['shield'])]
 size_tot=sum(Size)
 dx=0.2
-B_EOL=30000 # MWD/MTU
+B_EOL=36500 # MWD/MTU
 B_BOL=0
-B_rho=4.9725e-7
-hw_reactor=Reactor1D(H=Size,dx=1,P=P_1D,rho_ihm=B_rho,EOL=36500,BOL=0)
+B_rho=4.9725e-7 # MT/cm^3
+hw_reactor=Reactor1D(H=Size,dx=1,P=P_1D,rho_ihm=B_rho,EOL=B_EOL,BOL=0)
 
 T=15 # years
 dt=1 # year
@@ -442,11 +364,13 @@ for step in range(Nt):
     if step%14==0:
         print(f"Step {step}: k= {hw_reactor.k:.5f}, MaxBurnup={np.max(hw_reactor.Bt):.1f}")
 
-phi_BOL_th=hw_reactor.full_core('phi_th',0)
-phi_EOL_th=hw_reactor.full_core('phi_th',-1)
-phi_BOL_f=hw_reactor.full_core('phi_f',0)
-phi_EOL_f=hw_reactor.full_core('phi_f',-1)
+phi_BOL_th=hw_reactor.full_core('phi_phys_th',0)
+phi_EOL_th=hw_reactor.full_core('phi_phys_th',-1)
+phi_BOL_f=hw_reactor.full_core('phi_phys_f',0)
+phi_EOL_f=hw_reactor.full_core('phi_phys_f',-1)
 x=np.linspace(-size_tot,size_tot,len(phi_EOL_f))
+
+
 
 # plt.plot(x,phi_BOL_th/np.max(phi_BOL_th),'r--',label="Beginning of Life Flux (Thermal)")
 # plt.plot(x,phi_BOL_f/np.max(phi_BOL_f),'k-.',label="Beginning of Life Flux (Fast)")
@@ -459,6 +383,6 @@ plt.axvline(x=Size[0]+Size[1], color='gray', linestyle=':',label='Reflector Boun
 plt.axvline(x=-Size[0], color='gray',linestyle='--')
 plt.axvline(x=-(Size[0]+Size[1]),color='gray',linestyle=':')
 plt.legend()
-plt.ylabel('Normalized Flux')
+plt.ylabel('Neutron Flux')
 plt.xlabel('Reactor Position [cm]')
 plt.show()
